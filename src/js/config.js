9c5dbd310d9f0a8f0c0e7434d7c6ffa14d9b931f
Hernan Amaiz <hernanamaizp@gmail.com>
2021-07-29 22:05:15 -0400

    Primer commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e975ad47-f369-43bf-888f-a0f7ea21f5e9';
+export const MODAL_CLOSE_SEC = 2.5;
